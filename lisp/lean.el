45f32ff5c8321b2df79d4318bfbb832b8787c14b
YudaiKobayashi <yudaiiaduy5296@gmail.com>
2017-05-11 17:46:35 +0900

    fixed lean-mode for binary version

--- lisp/lean.el
+++ lisp/lean.el
@@ -1,4 +1,4 @@
 ;; You need to modify the following two lines:
-(setq lean-rootdir "~/lean")
-(setq load-path (cons "~/lean/src/emacs" load-path))
+(setq lean-rootdir "~/lean-3.1.0-darwin")
+(setq load-path (cons "~/lean-3.1.0-darwin/share/emacs/site-lisp/lean" load-path))
 (require 'lean-mode)
